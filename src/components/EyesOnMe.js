56ff50cb50a91c6660b8b15d07226b7992437b57
Johnson Joseph <jhonsonj88@gmail.com>
2020-01-22 01:42:08 +0000

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
 // Code EyesOnMe Component Here
+import React, {Component} from 'react';
+
+class EyesOnMe extends React.Component {
+
+
+handleFocus = () => {
+  console.log('Good!')
+}
+
+handleBlur = () => {
+  console.log('Hey! Eyes on me')
+}
+
+
+  render(){
+    return (
+      <div>
+      <button> onFocus={this.handleFocus} onBlur={this.handleBlur}> Button </button>
+      </div>
+    )
+  }
+
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
 // Code Keypad Component Here
+import React, {Component} from 'react',
+
+class Keypad extends React.Component {
+
+
+handleOnKeyUp = () => {
+  console.log('Entering password...')
+}
+
+
+  render(){
+    return (
+      <div>
+      <input type="password" onKeyUp={this.handleOnKeyUp} />
+      </div>
+    )
+  }
+
+}
